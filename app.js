7b6336c7350968b94ab6eff159727e66000fe3e4
Andrés Gutiérrez Ramírez <agutierrez.codes@gmail.com>
2021-10-11 18:47:03 +0200

    Importando paquetes de NPM

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const validator = require('validator')
 const getNotes = require('./nodes')
 
 const msg = getNotes()
-
 console.log(msg)
+
+console.log(validator.isEmail('andres@example.com'))
+console.log(validator.isURL('example.com'))
