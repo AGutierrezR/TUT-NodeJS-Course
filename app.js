9156501ec84d57a1762a192af13ecdf901f4e6b3
Andrés Gutiérrez Ramírez <agutierrez.codes@gmail.com>
2021-10-12 00:50:25 +0200

    Parsea Argumentoscon Yargs
    Con esto podemos colocar comandos luego del nombre del archivo
    
    node app.js add
    node app.js remove
    node app.js read
    node app.js list

--- app.js
+++ app.js
@@ -1,15 +1,47 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./nodes')
 const log = console.log
 
-const command = process.argv[2]
+// Customize yargs version
+yargs.version('1.1.0')
 
-log(process.argv)
+// add, remove, read, list
 
-if (command === 'add') {
-  log('Adding note!')
-}
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: () => {
+    log('Adding a new note!')
+  }
+})
 
-if (command === 'remove') {
-  log('Removing note!')
-}
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: () => {
+    log('Removing a note!')
+  }
+})
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: () => {
+    log('Reading a note!')
+  }
+})
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List your notes',
+  handler: () => {
+    log('Listing out all notes!')
+  }
+})
+
+log(yargs.argv)
