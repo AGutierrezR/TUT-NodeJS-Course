6a255378daa5ade372b3deae0587a0308bcf1725
Andrés Gutiérrez Ramírez <agutierrez.codes@gmail.com>
2021-11-11 19:53:53 +0100

    Paginas dinamicas con templating (handlerbars)
    Se debe usar el `app.set('view engine', 'hbs')` para indicarle a express cual sera el template engine que se usara
    
    Para los templates se debe usar el `app.get()` y en el se usara el `res.render()` donde se le pasar el nombre del template y un objeto con los datos de la pagina.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,9 +2,30 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-
 const publicDirPath = path.join(__dirname, '../public')
+
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirPath))
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Andres Gutierrez',
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About me',
+    name: 'Andres Gutierrez',
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    message: 'This is a helpful message.',
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send({
