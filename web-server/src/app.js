1aca2cea2cbe2a74e6f6de0aae8eb74971e40af0
Andrés Gutiérrez Ramírez <agutierrez.codes@gmail.com>
2021-11-11 18:03:47 +0100

    Serviendo HTML y JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,31 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
-  res.send('Hello express!')
+  res.send('<h1>Hello express!</h1>')
 })
 
 app.get('/help', (req, res) => {
-  res.send('Help page!')
+  res.send([
+    {
+      name: 'Andres',
+      age: 32,
+    },
+    {
+      name: 'Isabel',
+      age: 25,
+    },
+  ])
 })
 
 app.get('/about', (req, res) => {
-  res.send('About page!')
+  res.send('<h1>About page!</h1>')
 })
 
 app.get('/weather', (req, res) => {
-  res.send('Your weather!')
+  res.send({
+    forecast: 'It is cool!',
+    location: 'Valencia',
+  })
 })
 
 app.listen(3000, () => {
