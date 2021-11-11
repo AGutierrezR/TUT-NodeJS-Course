5e1a0e7738e1418cee48771a9c5844d4b3e4c372
Andrés Gutiérrez Ramírez <agutierrez.codes@gmail.com>
2021-11-11 15:28:05 +0100

    Trabajando con async

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,18 +2,38 @@ const fetch = require('node-fetch')
 require('dotenv').config()
 
 const API_KEY = process.env.API_KEY
-const geoCodeURL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=London&aqi=no`
+const geoCode = async (address) => {
+  return await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${address}&aqi=no`
+  )
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.error) {
+        throw res.error
+      }
+      return res
+    })
+    .then(({ location }) => {
+      return {
+        name: location.name,
+        country: location.country,
+        lat: location.lat,
+        lon: location.lon,
+      }
+    })
+}
 
-fetch(geoCodeURL)
-  .then((res) => res.json())
-  .then((res) => {
-    if (res.error) {
-      throw res.error
-    }
+geoCode('Venezuela').then(console.log).catch(console.log)
 
-    return res
-  })
-  .then((body) => console.log(body))
-  .catch((err) => {
-    console.log(err.message)
-  })
+// fetch(geoCodeURL)
+//   .then((res) => res.json())
+//   .then((res) => {
+//     if (res.error) {
+//       throw res.error
+//     }
+//     return res
+//   })
+//   .then((body) => console.log(body))
+//   .catch((err) => {
+//     console.log(err.message)
+//   })
